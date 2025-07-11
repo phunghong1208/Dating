c850d92963d7132a94d1f16d7131b1670bc356ef
BachaSoft <bachasoft02@BachaSofts-Mac-mini-2.local>
2025-07-11 16:19:22 +0700

    app-service

--- app-service/databases/entities/index.js
+++ app-service/databases/entities/index.js
@@ -1,23 +1,18 @@
 'use strict';
 
-const users = require('./users');
 const areas = require('./areas');
 const schools = require('./schools');
-const jobs = require('./jobs');
 const jobTitles = require('./jobTitles');
 const packages = require('./packages');
 const reasons = require('./reasons');
 const topics = require('./topics');
-const commons = require('./commons');
 const customers = require('./customers');
-const statics = require('./statics');
 const boosts = require('./boosts');
-const payments = require('./payments');
 const activities = require('./activities');
+const reports = require('./reports');
 const channels = require('./channels');
 const userChannels = require('./userChannels');
 const messages = require('./messages');
-const reports = require('./reports');
 const sockets = require('./sockets');
 const devices = require('./devices');
 const images = require('./images');
@@ -29,19 +24,14 @@ const reasonaccounts = require('./reasonaccounts');
 const reportaccounts = require('./reportaccounts');
 
 module.exports = {
-  users,
   areas,
   schools,
-  jobs,
   jobTitles,
   packages,
   reasons,
   topics,
-  commons,
-  statics,
   customers,
   boosts,
-  payments,
   activities,
   reports,
   channels,
